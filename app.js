5ab9dae92285954ba38572acdbbaac1921862946
Niko <niko.wah.hn@gmail.com>
2016-07-12 22:55:26 +0800

    add basic routes to post door open event to the IOT device

--- app.js
+++ app.js
@@ -16,7 +16,26 @@ var device = awsIot.device({
 	certPath: process.env.AWS_IOT_CERT_PATH,
 	caPath: process.env.AWS_IOT_CA_PATH,
 	clientId: process.env.AWS_IOT_CLIENT_ID,
-	region: process.env.AWS_REGION,
-	reconnectPeriod: 1500
+	region: process.env.AWS_REGION
 });
 
+device.on('connect', function(){
+	console.log('mqtt connected');
+	device.subscribe('door');
+});
+
+router.route('/open')
+	.post(function (request, response) {
+		device.publish('door', JSON.stringify({
+			event: 'open'
+		}));
+
+		response.send('door open event sent');
+	});
+
+app.use(router);
+app.use('/', express.static(__dirname + '/public'));
+
+server.listen(port, hostname, function(){
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
